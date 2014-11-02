07deebe93be9dd5276ae01b8b132771b875a0aa4
TickTakashi <TickTakashi@github.com>
2014-11-02 12:08:55 -0800

    Remove examples from erorr calculation.

--- CalculateError.m
+++ CalculateError.m
@@ -1,11 +1,11 @@
-function [ result ] = CalculateError(examples, targets, predicted_targets)
+function [ result ] = CalculateError(targets, predicted_targets)
     total = 0;
-    for i=1:numel(examples),
+    for i=1:numel(targets),
         if (targets(i) ~= predicted_targets(i))
             total = total + 1;
         end
     end
     
-    result = total / numel(examples);
+    result = total / numel(targets);
 end
 
