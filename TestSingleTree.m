35cb2d61f23b83565d9fe994b0274aedc444af01
Alexandros Mousafeiris <alexmsf@gmail.com>
2014-11-02 20:41:26 +0000

    Slight changes to recursive call. Still doesn't work

--- TestSingleTree.m
+++ TestSingleTree.m
@@ -6,7 +6,7 @@ end
 
 
 function [binary_label, depth] = SingleTreeHelper(tree, example, given_depth)
-    while ~isempty(tree.class)
+    if isempty(tree.class)
         attribute = example(tree.op);
             SingleTreeHelper(tree.kids{attribute+1}, example, given_depth+1);
     end
