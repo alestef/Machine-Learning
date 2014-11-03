850ba612cbf8a3d70abe9c7d2cd489da7ad5b9f1
TickTakashi <TickTakashi@github.com>
2014-11-02 18:04:05 -0800

    Add averages for confusion matrices and classification metrics

--- ConfusionMatrix.m
+++ ConfusionMatrix.m
@@ -1,26 +1,7 @@
-function cmatrix = ConfusionMatrix(data_set_filename)
-    load(data_set_filename);
-
-    T = cell(6,1);
-
-    cutoff = round(numel(y)/10)*9;
-
-    training_examples = x(1:cutoff,:);
-    training_targets = y(1:cutoff);
-    
-    real_targets = y(cutoff+1:numel(y));
-
-    for i=1:6 
-        T{i} = struct('kids',[],'op',[],'class',[]);
-        T{i} = DecisionTree(i, training_examples, training_targets);
-    end
-
-    predictions = TestTrees(T,x(cutoff+1:numel(y),:));
-
-    cmatrix = zeros(6,6);
-
-    for i=1:numel(real_targets)
-        cmatrix(real_targets(i), predictions(i)) = cmatrix(real_targets(i), predictions(i)) + 1;
+function cmatrix = ConfusionMatrix(real, pred)
+    cmatrix = zeros(6, 6);
+    for i=1:numel(real)
+        cmatrix(real(i), pred(i)) = cmatrix(real(i), pred(i)) + 1;
     end
 end
 
--- CrossValidateDecisionTrees.m
+++ CrossValidateDecisionTrees.m
@@ -1,6 +1,7 @@
-function [ average_error ] = CrossValidateDecisionTrees(data_set) 
+function [ avg_error, avg_confusion_matrix, avg_metrics ] = CrossValidateDecisionTrees(data_set) 
     load(data_set);
-    total_error = 0;
+    tot_error = 0;
+    tot_cmatrix = zeros(6, 6);
     for i = 1:NUM_FOLDS()
         [t_x, t_t, v_x, v_t] = Fold(i, x, y);
         trees = cell(6,1);
@@ -10,9 +11,12 @@ function [ average_error ] = CrossValidateDecisionTrees(data_set)
         end
         
         p_t = TestTrees(trees, v_x);
-        total_error = total_error + CalculateError(v_t, p_t);
+        tot_error = tot_error + CalculateError(v_t, p_t);
+        tot_cmatrix = tot_cmatrix + ConfusionMatrix(v_t, p_t);
     end
     
-    average_error = total_error / NUM_FOLDS();
+    avg_error = tot_error / NUM_FOLDS();
+    avg_confusion_matrix = tot_cmatrix / NUM_FOLDS();
+    avg_metrics = ClassificationMetrics(avg_confusion_matrix);
 end
 
