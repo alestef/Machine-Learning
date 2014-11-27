b554f3f899897ef51fc2994c32a820bdaac6b618
Nicolas Vayias <nv811@corona34.doc.ic.ac.uk>
2014-11-27 20:21:20 +0000

    implements similarity for the 3 metrics

--- casesSimilarity.m
+++ casesSimilarity.m
@@ -5,8 +5,8 @@ if( measure.STRCMP('length') )
     
 elseif( measure.STRCMP('matchAU') )
     % matching_AUs / avg_vec_length
-    
+    similarity = intersect(a,b).length / ( (a.length + b.length) / 2)
 else
     % matching_AUs / total_AUs
+    similarity = intersect(a,b).length / union(a,b).length
 end
-
