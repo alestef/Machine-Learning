6625a828654cd9b7e09ed6cdede97bf117442268
Nicolas Vayias <nv811@corona27.doc.ic.ac.uk>
2014-11-28 17:47:03 +0000

    Similarity Metrics now Include L1-norm and L2-norm(Euclidean)

--- casesSimilarity.m
+++ casesSimilarity.m
@@ -1,12 +1,45 @@
 function [ similarity ] = casesSimilarity( a, b, measure )
 
-if( measure.STRCMP('length') )
-   similarity = max(a.length,b.length) / min(a.length,b.length)
+if( measure.STRCMP('length') ) %euclidean distance(L2-norm)
+   vector1 = zeros(1,45);
+   vector2 = zeros(1,45);
+   
+   for i=1:length(a.problem)
+    vector1(a.problem(i)) = 1;
+   end
+   
+   for i=1:length(b.problem)
+    vector2(b.problem(i)) = 1;
+   end
+   
+   sum = 0;
     
+   for i=1:45
+    sum = sum + ( vector1(i)-vector2(i) )^2;
+   end
+   
+   similarity = sqrt(sum);  
+   
 elseif( measure.STRCMP('matchAU') )
     % matching_AUs / avg_vec_length
-    similarity = intersect(a,b).length / ( (a.length + b.length) / 2)
-else
-    % matching_AUs / total_AUs
-    similarity = intersect(a,b).length / union(a,b).length
+    similarity = intersect(a.problem,b.problem).length / ( (a.problem.length + b.problem.length) / 2)
+else %City-block(Manhattan) distance(L1-norm)
+   vector1 = zeros(1,45);
+   vector2 = zeros(1,45);
+
+   for i=1:length(a.problem)
+    vector1(a.problem(i)) = 1;
+   end
+   
+   for i=1:length(b.problem)
+    vector2(b.problem(i)) = 1;
+   end
+
+   sum = 0;
+    
+   for i=1:45
+    sum = sum + abs(vector1(i)-vector2(i));
+   end
+   
+   similarity = sum;  
 end
