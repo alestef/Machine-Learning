84138405f390d5c67b70c3d61911525e7278b24d
Alexandros Mousafeiris <alexmsf@gmail.com>
2014-12-01 04:22:10 +0000

    Added snippet for t-test data gathering.

--- CrossValidateDecisionTrees.m
+++ CrossValidateDecisionTrees.m
@@ -1,7 +1,9 @@
-function [ avg_error, avg_confusion_matrix, avg_metrics ] = CrossValidateDecisionTrees(data_set) 
+function [ avg_error, avg_confusion_matrix, avg_metrics, other_metrics ] = CrossValidateDecisionTrees(data_set) 
     load(data_set);
     tot_error = 0;
     tot_cmatrix = zeros(6, 6);
+    curr_cmatrix = zeros(6, 6);
+    other_metrics = zeros(10, 6);
     for i = 1:NUM_FOLDS()
         [t_x, t_t, v_x, v_t] = Fold(i, x, y);
         trees = cell(6,1);
@@ -11,8 +13,19 @@ function [ avg_error, avg_confusion_matrix, avg_metrics ] = CrossValidateDecisio
         end
         
         p_t = TestTrees(trees, v_x);
+        
+        %Other metrics stuff
+        %Used in t-tests
+        curr_cmatrix = ConfusionMatrix(v_t, p_t);
+        for j = 1:6
+            tp_tn = sum(diag(curr_cmatrix));
+            tp_tn_fp_fn = tp_tn + sum(curr_cmatrix(:,j)) + sum(curr_cmatrix(j,:));
+            other_metrics(i,j) = tp_tn/tp_tn_fp_fn;
+        end
+        %End of other metrics stuff
+        
         tot_error = tot_error + CalculateError(v_t, p_t);
-        tot_cmatrix = tot_cmatrix + ConfusionMatrix(v_t, p_t);
+        tot_cmatrix = tot_cmatrix + curr_cmatrix;
     end
     
     avg_error = tot_error / NUM_FOLDS();
